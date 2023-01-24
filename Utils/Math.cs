c7c5f72cacfc6fd49cd85f9e74fed93c8fac7fed
Raymond.Meara-Klinge.Student <112421067+Raymond-Meara-Klinge@users.noreply.github.com>
2023-01-24 17:55:14 +0000

    power made i guess

--- Utils/Math.cs
+++ Utils/Math.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+public class Matz
+{
+
+    public void Main(string[] args)
+    {
+
+    }
+
+    public int Power(int num1, int powerOf)
+    {
+        int result = num1;
+        int[] newArray = new int[powerOf];
+        if (powerOf > 0)
+        {
+            foreach (int number in newArray)
+            {
+                result *= num1;
+            }
+
+        }
+        return result;
+    }
+}
