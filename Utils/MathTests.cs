03640d87a47acb1a815306ed09122d3011a3b006
Raymond.Meara-Klinge.Student <112421067+Raymond-Meara-Klinge@users.noreply.github.com>
2023-01-27 18:28:15 +0000

    Fixed answer for circle area to be more accurate

--- Utils/MathTests.cs
+++ Utils/MathTests.cs
@@ -15,7 +15,7 @@ public class MathTests
         Assert.Equal(1467, Matz.Multiply(3, 489));
         Assert.Equal(8008135, Matz.Divide(16016270, 2));
         Assert.Equal(12, Matz.AreaOfRectangle(3, 4));
-        Assert.Equal(1134.11494795, Matz.FindCircleArea(19.0));
+        Assert.Equal(1134.11494794599, Matz.FindCircleArea(19.0));
         Assert.Equal(3204, Matz.FindRectPrismArea(89, 3, 12));
         Assert.Equal(967, Matz.Negate(-967));
     }
